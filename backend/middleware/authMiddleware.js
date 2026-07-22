e3173736561aa4afbd0bdc1ae4119debc8f1597b
joshua-3-byte <omondijoshua5674@gmail.com>
2026-07-22 22:31:52 +0300

    Add auth middleware debug logs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,30 +2,41 @@ import jwt from 'jsonwebtoken'
 import User from "../models/User.js"
 
 
-export const protectRoute = async (req,res,next) => {
-try {
-  const accessToken = req.cookies.accessToken
+export const protectRoute = async (req, res, next) => {
+  try {
+    console.log("========== protectRoute ==========");
+    console.log("Cookies:", req.cookies);
 
-  if(!accessToken) {
-    return res.status(401).json({message:'Unauthorized- No Access Token Provided'})
+    const accessToken = req.cookies.accessToken;
+    console.log("Access Token:", accessToken);
+
+    if (!accessToken) {
+      return res.status(401).json({
+        message: "Unauthorized - No Access Token Provided",
+      });
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    console.log("Decoded Token:", decoded);
+
+    const user = await User.findById(decoded.userId).select("-password");
+    console.log("User:", user);
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User Not Found",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in ProtectRoute Middleware:", error);
+    res.status(401).json({
+      message: "Unauthorized! Invalid Access Token",
+    });
   }
-
-  const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-  
-  const user = await User.findById(decoded.userId).select('-password')
-
-  if(!user){
-    return res.status(401).json({message:'User Not Found'})
-  } 
-  
-  req.user = user
-  next()
-
-} catch (error) {
-  console.error('Error in ProtectRoute Middleware', error)
-  res.status(401).json({message: 'Unauthorized! Invalid Access Token'})
-}
-}
+};
 
 export const adminRoute = (req,res,next) =>  {
 if (req.user && req.user.role === 'admin') {
