6da843d6c497c09aaa4c0803b2f7e0346f620715
joshua-3-byte <omondijoshua5674@gmail.com>
2026-07-22 18:56:41 +0300

    Remove frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,7 @@ const PORT = process.env.PORT || 5001
 
 const __dirname = path.resolve()
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  })
-}
 
 connectDB().then(() => {
 app.listen(PORT, () => {
