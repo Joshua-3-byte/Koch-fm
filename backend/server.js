5eeb7d69b0329bf0cfea04a95307ff176fe08d47
joshua-3-byte <omondijoshua5674@gmail.com>
2026-07-22 21:53:05 +0300

    Update CORS for Netlify domain

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://koch-fm-2.onrender.com",
+      "https://kochfm.netlify.app",
     ],
     credentials: true,
   })
